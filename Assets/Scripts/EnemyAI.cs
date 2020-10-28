0aa4a07b6c7cbf55a06a09efe9bf9a07b861ab0c
asotelo47 <anthony.sotelo559@myci.csuci.edu>
2020-10-28 02:20:47 -0700

    Added Flying Enemy Type and Enemy Spawner
    Uses A* pathfinding to create a flying enemy that chases the player around obstacles as well as enemy spawners that run coroutines to spawn enemies after a certain amount of time

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+
+
+    public Transform target;
+    public float speed = 400.0f;
+    public float nextWaypointDistance = 3.0f;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndofPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rigidB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        seeker = GetComponent<Seeker>();
+        rigidB = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.25f);
+    }
+
+    void UpdatePath()
+    {
+        if(Vector3.Distance(rigidB.position, target.position) > 1.0f)
+        {
+            seeker.StartPath(rigidB.position, target.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndofPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndofPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rigidB.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rigidB.AddForce(force);
+
+        float distance = Vector2.Distance(rigidB.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+}
