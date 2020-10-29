59d06d12a75db908b9fe0a8f78850707fe5c1820
true soria <true.soria601@myci.csuci.edu>
2020-10-29 00:19:43 -0700

    Integration of Zhili's Stuff
    - the knight dude

--- Assets/Scripts/EnemyAIDemo.cs
+++ Assets/Scripts/EnemyAIDemo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAIDemo : MonoBehaviour
 {
 
 
@@ -17,6 +17,8 @@ public class EnemyAI : MonoBehaviour
 
     Seeker seeker;
     Rigidbody2D rigidB;
+    private Animator _anim;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,7 @@ public class EnemyAI : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player").transform;
         seeker = GetComponent<Seeker>();
         rigidB = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
 
         InvokeRepeating("UpdatePath", 0f, 0.25f);
     }
@@ -65,7 +68,14 @@ public class EnemyAI : MonoBehaviour
 
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rigidB.position).normalized;
         Vector2 force = direction * (speed * Time.deltaTime);
-
+        
+        _anim.SetFloat("Speed", Mathf.Abs(rigidB.velocity.x));
+        if (Mathf.Abs(rigidB.velocity.x) > 0.1f)
+        {
+            Vector3 lscale = gameObject.transform.localScale; 
+            gameObject.transform.localScale = new Vector3(Mathf.Sign(rigidB.velocity.x) * Mathf.Abs(lscale.x), lscale.y, 1);
+        }
+        
         rigidB.AddForce(force);
 
         float distance = Vector2.Distance(rigidB.position, path.vectorPath[currentWaypoint]);
